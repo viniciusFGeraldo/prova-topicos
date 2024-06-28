73132434d32f9d39981bbf691356c405d9b3b1a8
fran8ctba <geraldo.fvinicius@gmail.com>
2024-06-28 11:25:19 -0300

    prova topicos

--- api-prova/src/App.tsx
+++ api-prova/src/App.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import TarefaListar from './components/tarefa-lista';
 import TarefaCadastrar from './components/tarefa-cadastrar';
+import TarefaAlterar from './components/tarefa-alterar';
 
 function App() {
   return (
     <div>
       <TarefaListar></TarefaListar>
       <TarefaCadastrar></TarefaCadastrar>
+      <TarefaAlterar></TarefaAlterar>
     </div>
   );
 }
